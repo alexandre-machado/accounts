c8362bafcf648a03e12ca82aca4cd13fc63e6c98
gBritz <gbritzv@gmail.com>
2015-09-01 00:26:12 -0300

    Removing comments

--- build.bat
+++ build.bat
@@ -1,23 +1,7 @@
-version: 1.0.{build}
-
-os: Visual Studio 2015 RC
-
-# config folder
 pushd %cd%
 cd %userprofile%\.dnx\runtimes\dnx*\bin
 set path=%path%;%cd%
 popd
 
-# restore and build web project
 cmd /c dnu restore
 cmd /c dnu build web
-
-# execute tests
-# cmd /c dnu build Tests --configuration Release
-# cmd /c dnx Tests test
-
-# starting app in docker
-# choco install docker -y
-# set docker_host=tcp://tbr.cloudapp.net:2375
-# docker build -t api Api
-# docker run -t -d -p 5004:5004 api
