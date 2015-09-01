e62c8490af1e8c49b8637410019e73a53c5b2e74
gBritz <gbritzv@gmail.com>
2015-08-31 23:56:16 -0300

    Appveyor config and build dnx script

---
+++ build.bat
@@ -0,0 +1,17 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+cmd /c %userprofile%\.dnx\bin\dnvm install latest
+pushd %cd%
+cd %userprofile%\.dnx\runtimes\dnx*\bin
+set path=%path%;%cd%
+popd
+cmd /c dnu restore
+cmd /c dnu build web
+
+#cmd /c dnu build Tests --configuration Release
+#cmd /c dnx Tests test
+
+#starting app in docker
+#choco install docker -y
+#set docker_host=tcp://tbr.cloudapp.net:2375
+#docker build -t api Api
+#docker run -t -d -p 5004:5004 api
