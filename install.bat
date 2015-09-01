05f2edb1b590596a716c247d99a0190a9e54c2e3
gBritz <gbritzv@gmail.com>
2015-09-01 10:59:22 -0300

    Create bat script to install dnvm

---
+++ install.bat
@@ -0,0 +1,6 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+cmd /c %userprofile%\.dnx\bin\dnvm install latest
+pushd %cd%
+cd %userprofile%\.dnx\runtimes\dnx*\bin
+set path=%path%;%cd%
+popd
