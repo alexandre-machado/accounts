bc86f33e2906932da8b74440b5b4cc321c0af188
gBritz <gbritzv@gmail.com>
2015-09-01 00:04:56 -0300

    comments build script

--- build.bat
+++ build.bat
@@ -1,12 +1,18 @@
+#install dnvm
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 cmd /c %userprofile%\.dnx\bin\dnvm install latest
+
+#config folder
 pushd %cd%
 cd %userprofile%\.dnx\runtimes\dnx*\bin
 set path=%path%;%cd%
 popd
+
+#restore and build web project
 cmd /c dnu restore
 cmd /c dnu build web
 
+#execute tests
 #cmd /c dnu build Tests --configuration Release
 #cmd /c dnx Tests test
 
