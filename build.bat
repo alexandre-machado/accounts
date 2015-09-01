7ee82ab09bbec1368887dd22b474e9e74d818ffc
gBritz <gbritzv@gmail.com>
2015-09-01 02:49:05 -0300

    Remove bat file and adding content into yml

--- build.bat
+++
@@ -1,9 +0,0 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
-cmd /c %userprofile%\.dnx\bin\dnvm install latest
-pushd %cd%
-cd %userprofile%\.dnx\runtimes\dnx*\bin
-set path=%path%;%cd%
-popd
-
-cmd /c dnu restore
-cmd /c dnu build src\Accounts.Web
