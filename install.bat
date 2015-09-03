e0d5a8474d3ceb258e6a7bbe32e45f5331744ecb
gBritz <gbritzv@gmail.com>
2015-09-03 00:29:20 -0300

    install specific version of dnx

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
-cmd /c %userprofile%\.dnx\bin\dnvm install latest
+cmd /c %userprofile%\.dnx\bin\dnvm install 1.0.0-beta6
 pushd %cd%
 cd %userprofile%\.dnx\runtimes\dnx*\bin
 set path=%path%;%cd%
