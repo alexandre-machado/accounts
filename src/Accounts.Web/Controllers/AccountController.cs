ef5d60569841009f1636af8639dba458d163c977
Alexandre Machado <alexandre@machado.cc>
2015-09-08 12:42:58 -0300

    feat: add LoginViewModel

--- src/Accounts.Web/Controllers/AccountController.cs
+++ src/Accounts.Web/Controllers/AccountController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNet.Authentication.Cookies;
-using Microsoft.AspNet.Mvc;
-using System.Collections.Generic;
-using System.Security.Claims;
+﻿using Microsoft.AspNet.Mvc;
+using Models;
+using System.Threading.Tasks;
 
 namespace Accounts.Web.Controllers
 {
     public class AccountController : BaseController
     {
+        [HttpGet]
         [AllowAnonymous]
         public IActionResult Login()
         {
+            //var claims = new List<Claim> { new Claim(ClaimTypes.Name, model.Username) };
+            //Context.Response.SignIn(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(new ClaimsIdentity(claims, "Cookies")));
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            // se logar retorna para URL
+            return RedirectToLocal(returnUrl);
+
             //var claims = new List<Claim> { new Claim(ClaimTypes.Name, model.Username) };
             //Context.Response.SignIn(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(new ClaimsIdentity(claims, "Cookies")));
             return View();
@@ -24,6 +38,18 @@ namespace Accounts.Web.Controllers
         public IActionResult AccessDenied()
         {
             return View();
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
         }
     }
 }
